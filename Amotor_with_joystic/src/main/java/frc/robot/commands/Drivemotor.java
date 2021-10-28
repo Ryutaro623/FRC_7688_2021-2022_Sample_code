9273fa4caf261415c8c05efb43b2fa99593fb5b0
Ryutaro <ryutarounozawa@icloud.com>
2021-10-28 00:52:00 -0700

    co

---
+++ Amotor_with_joystic/src/main/java/frc/robot/commands/Drivemotor.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Motor;
+
+public class Drivemotor extends CommandBase {
+  /** Creates a new Drivemotor. */
+  private final Motor mt;
+  public Drivemotor(Motor mtt) {
+    mt = mtt;
+    addRequirements(mt);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    mt.Drive(RobotContainer.joystick.getRawAxis(5));
+    SmartDashboard.putNumber("joystic value", RobotContainer.joystick.getRawAxis(5));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    mt.Drive(0);
+    SmartDashboard.putNumber("joystic value", 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
